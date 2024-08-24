218f0341b8876b9b28f393d992f7132165cacc51
manishverma45 <146928373+beast-45@users.noreply.github.com>
2024-08-24 16:57:42 +0530

    Update 2442-number-of-arithmetic-triplets.cpp

--- 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.cpp
+++ 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.cpp
@@ -10,9 +10,35 @@ public:
         }
         for(auto it : st)
         {
-            int sl = it-diff;
-            if(st.find(sl) != st.end()  && st.find(sl-diff) != st.end()) count++;
+            int sl = it-diff; // it = largest  , sl = Second Largest 
+            if(st.find(sl) != st.end()  && st.find(sl-diff) != st.end()) count++; // Third Largest = sl-diff
         }
         return count;
+    }
+};
+
+
+// BRUTEFORCE : 
+
+class Solution {
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff) {
+        int n = nums.size();
+        int tcount = 0;
+        for(int i=0 ; i<n-2 ; i++)
+        {
+            for(int j=i+1 ; j<n-1 ; j++)
+            {
+                if(nums[j]-nums[i]==diff)
+                {
+                    for(int k=j+1 ; k<n ; k++)
+                    {
+                        if(nums[k]-nums[j]==diff) tcount++;
+                    }
+                }
+            }
+        }
+        return tcount;
+        
     }
 };
