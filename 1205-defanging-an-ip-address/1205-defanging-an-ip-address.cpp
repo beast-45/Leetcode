9e523015dd0fb715544514261e1e0838703fd7bf
manishverma45 <146928373+beast-45@users.noreply.github.com>
2024-08-26 17:57:46 +0530

    Time: 3 ms (23.28%), Space: 7.4 MB (37.36%) - LeetHub

---
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ansadd = "";
+        for(char &ch : address)
+        {
+            if(ch != '.') ansadd+=ch;
+            else
+            {
+                ansadd+='[';
+                ansadd+='.';
+                ansadd+=']';
+            }
+        }
+        return ansadd;
+        
+    }
+};
