f90b7977c1abd7587bf95d719ff5763ea296b514
manishverma45 <146928373+beast-45@users.noreply.github.com>
2024-09-03 10:33:21 +0530

    Time: 156 ms (50.86%), Space: 98.4 MB (41.81%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int &num : nums)
+        {
+            mp[num]++;
+        }
+        int minE = *min_element(begin(nums),end(nums));
+        int maxE = *max_element(begin(nums),end(nums));
+
+        int i=0;
+        for(int num=minE ; num<=maxE ; num++)
+        {
+            while(mp[num]>0)
+            {
+                nums[i]=num;
+                i++;
+                mp[num]--;
+            }
+        }    
+        return nums;   
+    }
+};
