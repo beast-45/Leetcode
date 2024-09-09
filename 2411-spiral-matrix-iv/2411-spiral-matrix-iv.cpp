765f1983de5ef16a24960287d8968600c4c15590
manishverma45 <146928373+beast-45@users.noreply.github.com>
2024-09-09 14:38:01 +0530

    Time: 154 ms (84.29%), Space: 130.6 MB (65.45%) - LeetHub

---
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+        vector<vector<int>> matrix(m , vector<int>(n , -1));
+
+        int id=0 ;
+        int top = 0;
+        int down = m-1;
+        int left = 0 ;
+        int right = n-1;
+
+        while(top <= down && left <= right)
+        {
+            if(id == 0)
+            {
+                for(int i=left ; head != NULL && i<=right ; i++)
+                {
+                    matrix[top][i] = head->val;
+                    head = head->next;
+                }
+                top += 1;
+            }
+            if(id == 1)
+            {
+                for(int i=top ; head != NULL &&  i<= down ; i++)
+                {
+                    matrix[i][right] = head->val;
+                    head = head->next;
+                }
+                right--;
+            }
+            if(id == 2)
+            {
+                for(int i=right ; head != NULL && i>=left ; i--)
+                {
+                    matrix[down][i] = head->val;
+                    head = head->next;
+                }
+                down--;
+            }
+            if(id == 3)
+            {
+                for(int i=down ; head != NULL && i>=top ; i--)
+                {
+                    matrix[i][left] = head->val;
+                    head = head->next;
+                }
+                left++;
+            }
+            id = (id+1)%4;
+        }
+        return matrix;
+        
+    }
+};
