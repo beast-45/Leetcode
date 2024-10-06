9fd62dee33a921950fa47b091c03593a1e664a11
manishverma45 <146928373+beast-45@users.noreply.github.com>
2024-10-06 20:13:32 +0530

    Time: 0 ms (100%), Space: 8.7 MB (22.58%) - LeetHub

---
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool areSentencesSimilar(string s1, string s2) {
+        if(s1.length() < s2.length()) swap(s1,s2);
+        vector<string> vec1;
+        vector<string> vec2;
+
+        stringstream ss1(s1);
+        string token;
+        while(ss1 >> token)
+        {
+            vec1.push_back(token);
+        }
+
+        stringstream ss2(s2);
+        while(ss2 >> token)
+        {
+            vec2.push_back(token);
+        }
+
+        int i=0 , j = vec1.size()-1;
+        int k=0 , l = vec2.size()-1;
+
+        while(k<vec2.size() && i<vec1.size() && vec1[i] == vec2[k])
+        {
+            k++;
+            i++;
+        }
+        while(l>=k && vec2[l] == vec1[j])
+        {
+            l--;
+            j--;
+        }
+
+        if(l<k) return true;
+        return false;
+        
+    }
+};
