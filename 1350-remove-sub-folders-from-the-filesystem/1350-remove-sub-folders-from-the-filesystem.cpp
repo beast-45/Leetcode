b96d0289ed4ec3758863619dc21c6ed596edc2d4
manishverma45 <146928373+beast-45@users.noreply.github.com>
2024-10-25 13:52:37 +0530

    Time: 59 ms (92.86%), Space: 53.5 MB (64.56%) - LeetHub

---
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        sort(begin(folder), end(folder));
+        vector<string> result;
+
+        result.push_back(folder[0]); 
+
+        for(int i = 1; i < folder.size(); i++) {
+
+            string currFolder = folder[i];
+            string lastFolder = result.back();
+            lastFolder += '/';
+
+            if(currFolder.find(lastFolder) != 0) {
+                result.push_back(currFolder);
+            }
+        }
+
+        return result;
+
+    }
+};
