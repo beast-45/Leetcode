4f4a3ed2d8ce5de4928be0b03973e319510d72db
manishverma45 <146928373+beast-45@users.noreply.github.com>
2024-11-12 15:18:46 +0530

    Time: 59 ms (58.2%), Space: 91.3 MB (98.65%) - LeetHub

---
+++ 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int CBinarySearch(vector<vector<int>> &items , int queryPrice)
+    {
+        int l=0;
+        int r = items.size() - 1;
+        int mid;
+        int maxBeauty = 0;
+
+        while(l<=r)
+        {
+            mid = l + (r-l) / 2;
+            if(items[mid][0] > queryPrice)
+            {
+                r = mid - 1;
+            }
+            else
+            {
+                maxBeauty = max(maxBeauty , items[mid][1]);
+                l = mid+1;
+            }
+        }
+        return maxBeauty;
+    }
+    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
+        int n = items.size();
+        int m = queries.size();
+
+        vector<int> result(m);
+
+        sort(begin(items) , end(items));
+
+        int maxBeautySeen = 0;
+
+        for(int i=0 ; i<n ; i++)
+        {
+            maxBeautySeen = max(maxBeautySeen , items[i][1]);
+            items[i][1] = maxBeautySeen;
+        }
+        
+        for(int i=0 ; i<m ; i++)
+        {
+            int queryPrice = queries[i];
+            result[i] = CBinarySearch(items , queryPrice);
+        }
+
+        return result;
+    }
+};
