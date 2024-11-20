a446e82e89f335945e48c58b481be5dadb2afefc
manishverma45 <146928373+beast-45@users.noreply.github.com>
2024-11-20 09:27:42 +0530

    Time: 0 ms (100%), Space: 12.4 MB (13.1%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -2,10 +2,37 @@ class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p == NULL && q == NULL) return true;
+        if(p == NULL || q == NULL) return false;
 
-        if(p == NULL || q ==  NULL) return false;
-        if(p->val != q->val) return false;
+        queue<TreeNode*> q1;
+        queue<TreeNode*> q2;
 
-        return isSameTree(p->left , q->left) && isSameTree(p->right , q->right);
+        q1.push(p);
+        q2.push(q);
+
+        while(!q1.empty() && !q2.empty())
+        {
+            TreeNode* n1 = q1.front();
+            q1.pop();
+            TreeNode* n2 = q2.front();
+            q2.pop();
+
+            if(n1->val != n2->val) return false;
+
+            if(n1->left != NULL && n2->left != NULL)
+            {
+                q1.push(n1->left);
+                q2.push(n2->left);
+            }
+            else if(n1->left != NULL || n2->left != NULL) return false;
+            if(n1->right != NULL && n2->right != NULL)
+            {
+                q1.push(n1->right);
+                q2.push(n2->right);
+            }
+            else if(n1->right != NULL || n2->right != NULL) return  false;
+        }
+        return true;
+        
     }
 };
