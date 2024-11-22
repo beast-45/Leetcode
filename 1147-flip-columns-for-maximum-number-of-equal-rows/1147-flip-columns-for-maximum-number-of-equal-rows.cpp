30cae7b8e1f5ba1dc9ec33de9c6bbfd523bbf155
manishverma45 <146928373+beast-45@users.noreply.github.com>
2024-11-22 15:23:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int maxRows= 0;
+        
+        for(auto &currRow : matrix)
+        {
+            vector<int> inverted(n);
+            for(int col=0 ; col<n ; col++)
+            {
+                inverted[col] = currRow[col] == 0 ? 1 : 0;
+            }
+            int count = 0;
+            for(auto &row : matrix)
+            {
+                if(row == currRow || row == inverted) count += 1;
+            }
+            maxRow = max(maxRow , count);
+        }
+        return maxRow;
+    }
+};
