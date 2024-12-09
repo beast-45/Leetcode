6cc55dc4907e769be8ca613a3934c9453cb8450d
manishverma45 <146928373+beast-45@users.noreply.github.com>
2024-12-09 16:38:05 +0530

    Time: 6 ms (75%), Space: 126.5 MB (66.56%) - LeetHub

---
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        int m = queries.size();
+
+        vector<int> cumulativeSum(n,0);
+        cumulativeSum[0] = 0;
+
+        for(int i=1 ; i<n ; i++)
+        {
+            if(nums[i] % 2 == nums[i-1] % 2)
+            {
+                cumulativeSum[i] = cumulativeSum[i-1] + 1;
+            }
+            else
+            {
+                cumulativeSum[i] = cumulativeSum[i-1];
+            }
+        }
+        vector<bool> result;
+        int i = 0;
+        for(vector<int> &query : queries)
+        {
+            int start = query[0];
+            int end   = query[1];
+
+            cumulativeSum[end] - cumulativeSum[start] == 0 ? result.push_back(true) : result.push_back(false);
+            i++;
+        }
+        return result;
+        
+    }
+};
