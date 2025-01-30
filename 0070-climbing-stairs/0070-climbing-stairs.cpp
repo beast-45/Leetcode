f34b883bf6295539d03c2dcd3e746bda1f48612a
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-01-30 12:50:42 +0530

    Time: 0 ms (100%), Space: 7.9 MB (58.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int t[46];
-    int solve(int n)
-    {
-        if(n<0) return 0;
-        if(t[n] != -1) return t[n];
-        if(n==0) return 1;
-        
-        int oneStep = solve(n-1);
-        int twoStep = solve(n-2);
+    int solve(int n) {
+        if (n < 0) {
+            return 0;
+        }
+        if (n == 0) {
+            return 1;
+        }
+        if(t[n] != -1){
+            return t[n];
+        }
+
+        int oneStep = solve(n - 1);
+        int twoStep = solve(n - 2);
 
         return t[n] = oneStep + twoStep;
     }
     int climbStairs(int n) {
-        memset(t , -1 , sizeof(t));
+        memset(t,-1,sizeof(t));
         return solve(n);
-        
     }
 };
