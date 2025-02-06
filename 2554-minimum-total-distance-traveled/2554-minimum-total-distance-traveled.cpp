0bb5b405a054ab19c3e0746f3f2bbd7717cb6b5b
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-02-06 11:50:39 +0530

    Time: 135 ms (38.25%), Space: 70.1 MB (21.81%) - LeetHub

--- 2554-minimum-total-distance-traveled/2554-minimum-total-distance-traveled.cpp
+++ 2554-minimum-total-distance-traveled/2554-minimum-total-distance-traveled.cpp
@@ -1,25 +1,37 @@
-using ll = long long;
-
 class Solution {
 public:
+    long long solve(vector<int> &robot , vector<int> &factories , int i , int j , vector<vector<long long>> &dp){
+        if(i >= robot.size()){
+            return 0;
+        }
+        if(j >= factories.size()){
+            return 1e12;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        long long take = abs(robot[i]-factories[j]) + solve(robot , factories , i+1 , j+1 , dp);
+        long long skip = solve(robot , factories , i , j+1 , dp);
+
+        return dp[i][j] = min(take , skip);
+
+    }
     long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
-        sort(robot.begin(), robot.end());
-        sort(factory.begin(), factory.end());
-        vector<vector<ll>> f(robot.size(), vector<ll>(factory.size()));
-        function<ll(int i, int j)> dfs = [&](int i, int j) -> ll {
-            if (i == robot.size()) return 0;
-            if (j == factory.size()) return 1e15;
-            if (f[i][j]) return f[i][j];
-            ll ans = dfs(i, j + 1);
-            ll t = 0;
-            for (int k = 0; k < factory[j][1]; ++k) {
-                if (i + k >= robot.size()) break;
-                t += abs(robot[i + k] - factory[j][0]);
-                ans = min(ans, t + dfs(i + k + 1, j + 1));
+        sort(begin(robot),end(robot));
+        vector<int> factories;
+        for(auto &fac : factory){
+            int facIndex = fac[0];
+            int number   = fac[1];
+
+            for(int i=0 ; i<number ; i++){
+                factories.push_back(facIndex);
             }
-            f[i][j] = ans;
-            return ans;
-        };
-        return dfs(0, 0);
+        }
+        sort(begin(factories),end(factories));
+        int m = robot.size();
+        int n = factories.size();
+        vector<vector<long long>> dp(m+1 , vector<long long>(n+1 , -1));
+        return solve(robot , factories , 0 , 0 , dp);
     }
 };
