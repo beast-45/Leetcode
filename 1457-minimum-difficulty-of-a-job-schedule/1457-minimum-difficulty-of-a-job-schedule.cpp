9d7cad89e3780d059b1ed060ea247f071874f2ad
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-02-10 10:38:27 +0530

    Time: 31 ms (46.39%), Space: 10.2 MB (95.6%) - LeetHub

--- 1457-minimum-difficulty-of-a-job-schedule/1457-minimum-difficulty-of-a-job-schedule.cpp
+++ 1457-minimum-difficulty-of-a-job-schedule/1457-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
-    int t[301][11];
-    int solve(vector<int> &jd , int n , int idx , int d)
-    {
-        if(d==1)
-        {
-            int maxD = jd[idx];
-            for(int i=idx ; i<n ; i++)
-            {
-                maxD = max(maxD , jd[i]);
+    int dp[301][11];
+    int solve(vector<int> &jobDifficulty , int index , int days , int n){
+        if(days == 1){
+            int maxD = jobDifficulty[index];
+            for(int i=index ; i<n ; i++){
+                maxD = max(maxD , jobDifficulty[i]);
             }
             return maxD;
         }
-        if(t[idx][d] != -1)
-        {
-            return t[idx][d];
+        if(dp[index][days] != -1){
+            return dp[index][days];
         }
-
-        int maxD = jd[idx];
-        int finalResult = INT_MAX;
-
-        for(int i=idx ; i<=n-d ; i++)
-        {
-            maxD = max(maxD , jd[i]);
-            int result = maxD + solve(jd , n , i+1 , d-1);
-            finalResult = min(finalResult , result);
+        int maxD = jobDifficulty[index];
+        int minD = INT_MAX;
+        for(int i=index ; i<=n-days ; i++){
+            maxD = max(maxD , jobDifficulty[i]);
+            int diff = maxD + solve(jobDifficulty , i+1 , days-1 , n);
+            minD = min(minD , diff);
         }
-        return t[idx][d] = finalResult;
+        return dp[index][days] = minD;
     }
-    int minDifficulty(vector<int>& jd, int d) {
-        int n = jd.size();
-        if(n<d) return -1;
-        memset(t ,-1,sizeof(t));
-        return solve(jd ,n , 0 ,d);
+    int minDifficulty(vector<int>& jobDifficulty, int d) {
+        int n = jobDifficulty.size();
+        if(n < d){
+            return -1;
+        }
+        memset(dp,-1,sizeof(dp));
+        return solve(jobDifficulty , 0 , d , n);
     }
 };
