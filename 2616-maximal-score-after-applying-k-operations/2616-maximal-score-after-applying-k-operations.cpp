cd7135df793160c2448a6dd432c11ebd68c806fa
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-02-13 12:03:14 +0530

    Time: 70 ms (89.79%), Space: 76.1 MB (84.11%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -3,13 +3,11 @@ public:
     long long maxKelements(vector<int>& nums, int k) {
         long long score = 0;
         priority_queue<int> pq(begin(nums) , end(nums));
-        while(k--)
-        {
-            int maxE =  pq.top();
+        while(k--){
+            int largest =  pq.top();
             pq.pop();
-            score += maxE;
-            maxE = ceil(maxE/3.0);
-            pq.push(maxE);
+            score += largest;
+            pq.push(ceil(largest/3.0));
         }
         return score;
         
