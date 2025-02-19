cbd092f3ab0bda63189b189475b69cd9bfcd5d61
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-02-19 10:49:41 +0530

    Time: 3 ms (74.09%), Space: 8.1 MB (86.1%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    void solve(int n , string &temp , vector<string> &happyStrings , vector<char> &letters){
+    void solve(int n , string &temp , int &k , int &count , string &result){
         if(temp.length() == n){
-            happyStrings.push_back(temp);
+            count += 1;
+            if(count == k){
+                result = temp;
+            }
             return;
         }
-        for(int i=0 ; i<3 ; i++){
-            if(!temp.empty() && temp.back() == letters[i]){
+        for(char ch='a' ; ch<'d' ; ch++){
+            if(!temp.empty() && temp.back() == ch){
                 continue;
             }
-            temp.push_back(letters[i]);
-            solve(n , temp , happyStrings , letters);
+            temp.push_back(ch);
+            solve(n , temp , k , count , result);
             temp.pop_back();
         }
     }
     string getHappyString(int n, int k) {
         vector<char> letters{'a' , 'b' , 'c'};
         string temp;
-        vector<string> happyStrings;
-        solve(n , temp , happyStrings , letters);
-        if(k>happyStrings.size()){
-            return "";
-        }
-        return happyStrings[k-1];
+        string result = "";
+        int count = 0;
+        solve(n , temp , k  , count , result);
+        return result;
     }
 };
