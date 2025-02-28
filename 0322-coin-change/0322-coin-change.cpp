415c7fc858b5f1e681f69c1b27da6a4e7d9e0948
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-02-28 18:41:24 +0530

    Time: 30 ms (55.93%), Space: 13.7 MB (94.16%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> t;
-    int solve(vector<int> &coins , int amount , int idx){
-        if(idx >= coins.size() || amount<0) return 1000000;
-        if(amount == 0) return 0;
+    int dp[10001][13]; 
+    int solve(vector<int> &coins, int amount, int idx) {
+        if (amount == 0){
+            return 0;
+        }
+        if (idx >= coins.size() || amount < 0){
+            return 1e6;
+        }
 
-        if(t[idx][amount] != -1) return t[idx][amount];
+        if (dp[amount][idx] != -1){
+            return dp[amount][idx];
+        }
 
-        int take = 1 + solve(coins , amount-coins[idx] , idx);
-        int skip = solve(coins , amount , idx+1);
+        int take = 1 + solve(coins, amount - coins[idx], idx);
+        int skip = solve(coins, amount, idx + 1);
 
-        return t[idx][amount] = min(skip , take);
+        return dp[amount][idx] = min(take, skip);
     }
+
     int coinChange(vector<int>& coins, int amount) {
-        t = vector<vector<int>>(coins.size() , vector<int>(amount+1 , -1));
-        int minCoins = solve(coins , amount , 0);
-        return minCoins == 1000000 ? -1 : minCoins;
+        memset(dp, -1, sizeof(dp));
+        int minCoins = solve(coins, amount, 0);
+        return (minCoins >= 1e6) ? -1 : minCoins;
     }
 };
