4faf50e91b72aef80ab7c16061dab8bbd002cbc1
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-03-04 19:28:34 +0530

    Update 0210-course-schedule-ii.cpp

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+
+    // Using kahn's Algorithm (BFS)
     void bfs(unordered_map<int,vector<int>> &adj , vector<int> &indegree , vector<int> &result , int n , int &count){
         queue<int> q;
         for(int i=0 ; i<n ; i++){
@@ -35,5 +37,53 @@ public:
         }
         bfs(adj , indegree , result , n , count);
         return count != n ? vector<int>{} : result; 
+    }
+};
+
+//Using DFS
+class Solution {
+public:
+    bool hasCycle = false;
+    void dfs(unordered_map<int,vector<int>> &adj , int u , vector<bool> &vis , vector<bool> &inRec , vector<int> &result){
+        vis[u] = true;
+        inRec[u] = true;
+        for(int &v : adj[u]){
+            if(!vis[v]){
+                dfs(adj , v , vis , inRec , result);
+            }
+            else if(inRec[v]){
+                hasCycle = true;
+                return;
+            }
+        }
+        inRec[u] = false;
+        result.push_back(u);
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+        vector<bool> vis(n,false);
+        vector<bool> inRec(n,false);
+        vector<int> result;
+
+        unordered_map<int,vector<int>> adj;
+        for(vector<int> &pre : prerequisites){
+            int a = pre[0];
+            int b = pre[1];
+            adj[b].push_back(a);
+        }
+
+        for(int i=0 ; i<n ; i++){
+            if(!vis[i]){
+                dfs(adj , i , vis , inRec , result);
+            }
+        }
+        
+        if(hasCycle){
+            return {};
+        }
+        else{
+            reverse(begin(result),end(result));
+        }
+        return result;
     }
 };
