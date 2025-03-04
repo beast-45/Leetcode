6fad6783cc1284efe70a74892b18367a3723f6f8
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-03-04 18:25:10 +0530

    Time: 2 ms (80.16%), Space: 18.5 MB (49.7%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void bfs(unordered_map<int,vector<int>> &adj , vector<int> &indegree , vector<int> &result , int n , int &count){
+        queue<int> q;
+        for(int i=0 ; i<n ; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+                count += 1;
+            }
+        }
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            result.push_back(u);
+            for(int &v : adj[u]){
+                indegree[v]--;
+                if(indegree[v] == 0){
+                    q.push(v);
+                    count += 1;
+                }
+            }
+        }
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+        unordered_map<int,vector<int>> adj;
+        vector<int> result;
+        vector<int> indegree(n,0);
+        int count = 0;
+        for(auto &pre : prerequisites){
+            int a = pre[0];
+            int b = pre[1];
+            adj[b].push_back(a);
+            indegree[a]++;
+        }
+        bfs(adj , indegree , result , n , count);
+        return count != n ? vector<int>{} : result; 
+    }
+};
