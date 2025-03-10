96f755c6f41955582ac40645307d79ba4acc37bb
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-03-10 14:30:36 +0530

    Time: 0 ms (100%), Space: 17.7 MB (71.59%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -2,18 +2,34 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<int> result(n,INT_MAX);
+        unordered_map<int,vector<pair<int,int>>> adj;
+        for(auto& flight : flights){
+            int u = flight[0];
+            int v = flight[1];
+            int w = flight[2];
+            adj[u].push_back({v,w});
+        }
+        int count = 0; 
+        queue<pair<int,int>> q;
+        q.push({src,0});
         result[src] = 0;
-        for(int i=0 ; i<=k ; i++){
-            vector<int> temp = result;
-            for(auto &flight : flights){
-                int u = flight[0];
-                int v = flight[1];
-                int w = flight[2];
-                if(result[u] != INT_MAX && result[u] + w < temp[v]){
-                    temp[v] = result[u] + w;
+        while(!q.empty() && count <= k){
+            int s = q.size();
+            while(s--){
+                auto t = q.front();
+                q.pop();
+                int u = t.first;
+                int w = t.second;
+                for(auto &v : adj[u]){
+                    int nv = v.first;
+                    int nw = v.second;
+                    if(w+nw < result[nv]){
+                        result[nv] = w+nw;
+                        q.push({nv,result[nv]});
+                    }
                 }
             }
-            result = temp;
+            count += 1;
         }
         return result[dst] == INT_MAX ? -1 : result[dst];
     }
