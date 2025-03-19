682fba0a3ddbd95ef9b2c58a074c1d34dca1445f
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-03-19 19:11:51 +0530

    Time: 17 ms (66.53%), Space: 23.7 MB (79.3%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -3,34 +3,36 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
+
+        queue<pair<int,int>> q;
+        vector<vector<int>> directions{{1,0},{-1,0},{0,1},{0,-1},{-1,-1},{-1,1},{1,-1},{1,1}};
+
         if(grid[0][0] == 1 || grid[m-1][n-1] == 1){
             return -1;
         }
-        vector<vector<int>> directions{{-1,0},{1,0},{0,-1},{0,1},{-1,-1},{-1,1},{1,-1},{1,1}};
-        vector<vector<int>> result(m , vector<int>(n, INT_MAX));
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-        pq.push({1,{0,0}});
-        result[0][0] = 1;
-        while(!pq.empty()){
-            auto u = pq.top();
-            pq.pop();
-            int d = u.first;
-            int x = u.second.first;
-            int y = u.second.second;
-            for(auto &dir : directions){
-                int nx = x + dir[0];
-                int ny = y + dir[1];
-                int dist = 1;
-                if((nx >= 0 && nx < m && ny >= 0 && ny < n) && grid[nx][ny] == 0 && d+dist < result[nx][ny]){
-                    result[nx][ny] = d+dist;
-                    pq.push({result[nx][ny],{nx,ny}});
-
+        int path = 0;
+        q.push({0,0});
+        grid[0][0] = -1;
+        while(!q.empty()){
+            int s = q.size();
+            while(s--){
+                int x = q.front().first;
+                int y = q.front().second;
+                q.pop();
+                if(x == m-1 && y == n-1){
+                    return path + 1;
+                }
+                for(vector<int> &direction : directions){
+                    int nx = x + direction[0];
+                    int ny = y + direction[1];
+                    if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]==0){
+                        q.push({nx,ny});
+                        grid[nx][ny] = 1;
+                    }
                 }
             }
+            path+=1;
         }
-        if(result[m-1][n-1] == INT_MAX){
-            return -1;
-        }
-        return result[m-1][n-1]; 
+        return -1;
     }
 };
