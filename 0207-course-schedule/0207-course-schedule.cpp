1dd1e2f9673b4861a75675540b40bf4535bc183a
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-03-19 11:13:24 +0530

    Time: 3 ms (81.84%), Space: 19.3 MB (67.53%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,34 +2,33 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n = numCourses;
-        vector<int> indegree(n,0);
         queue<int> q;
+        vector<int> indegree(n,0);
         int count = 0;
-        unordered_map<int,vector<int>> adj;
-        for(auto &u : prerequisites){
-            int a = u[0];
-            int b = u[1];
+        vector<vector<int>> adj(n);
+        for(auto &prerequisite : prerequisites){
+            int a = prerequisite[0];
+            int b = prerequisite[1];
             adj[b].push_back(a);
-            indegree[a] += 1;
+            indegree[a]+=1;
         }
-
         for(int i=0 ; i<n ; i++){
-            if(indegree[i] == 0){
+            if(indegree[i]==0){
                 q.push(i);
-                count += 1;
+                count+=1;
             }
         }
         while(!q.empty()){
             int u = q.front();
             q.pop();
             for(int &v : adj[u]){
-                indegree[v]--;
+                indegree[v]-=1;
                 if(indegree[v] == 0){
                     q.push(v);
-                    count += 1;
+                    count+=1;
                 }
             }
         }
-        return count == n;
+        return count == n ? true : false;
     }
 };
