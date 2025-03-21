f49520e6f65ba6d98979f1073dc35a91690f3530
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-03-21 22:04:23 +0530

    Time: 155 ms (55.37%), Space: 217 MB (34.39%) - LeetHub

--- 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
@@ -1,47 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int swapNum(vector<int>& vec) {
-        int swaps = 0;
-        vector<int> sorted(begin(vec), end(vec));
-        sort(begin(sorted), end(sorted));
-        unordered_map<int, int> mp;
-
-        for(int i = 0; i < vec.size(); i++)
-        {
+    int swaps(vector<int> &vec){
+        int n = vec.size();
+        int op = 0;
+        vector<int> sortedVec(begin(vec),end(vec));
+        sort(begin(sortedVec),end(sortedVec));
+        unordered_map<int,int> mp;
+        for(int i=0 ; i<n ; i++){
             mp[vec[i]] = i;
         }
-
-        for(int i = 0; i < vec.size(); i++) {
-            if(vec[i] == sorted[i]) continue; 
-
-            int currIdx = mp[sorted[i]];
-            mp[vec[i]] = currIdx;
-            mp[vec[currIdx]] = i;
-            swap(vec[currIdx], vec[i]);
-            swaps++;
-        }
-        return swaps;
-    }
-
-    int minimumOperations(TreeNode* root) {
-        queue<TreeNode*> que;
-        que.push(root);
-        int result = 0;
-
-        while(!que.empty()) {
-            int n = que.size(); 
-            vector<int> vec;
-
-            while(n--) {
-                TreeNode* temp = que.front();
-                que.pop();
-                vec.push_back(temp->val);
-                if(temp->left) que.push(temp->left);
-                if(temp->right) que.push(temp->right);
+        for(int i=0 ; i<n ; i++){
+            if(vec[i] != sortedVec[i]){
+                int index = mp[sortedVec[i]];
+                mp[sortedVec[i]] = i;
+                mp[vec[i]] = index; 
+                swap(vec[i],vec[index]);
+                op += 1;
             }
-            result += swapNum(vec);
         }
-
-        return result;
+        return op;
+    }
+    int minimumOperations(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int operations = 0;
+        while(!q.empty()){
+            int n = q.size();
+            vector<int> level;
+            while(n--){
+                TreeNode* temp = q.front();
+                q.pop();
+                level.push_back(temp->val);
+                if(temp->left){
+                    q.push(temp->left);
+                }
+                if(temp->right){
+                    q.push(temp->right);
+                }
+            }
+            operations += swaps(level);
+        }
+        return operations; 
     }
 };
