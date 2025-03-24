5aa1322b8590b3705cc61d33c697aa0a398a069d
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-03-24 20:18:52 +0530

    Time: 0 ms (100%), Space: 9.5 MB (31.12%) - LeetHub

--- 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.cpp
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
-    char dfs(unordered_map<char,vector<char>> &adj , int uChar , vector<bool> &vis){
-        vis[uChar-'a'] = true;
-        char minChar = uChar;
-        for(char &vChar : adj[uChar]){
-            if(!vis[vChar-'a']){
-                minChar = min(minChar,dfs(adj,vChar,vis));
-            }
+    int find(int x , vector<int> &parent){
+        if(x == parent[x]){
+            return x;
         }
-        return minChar;
+        return parent[x] = find(parent[x],parent);
+    }
+    void unionf(int x , int y , vector<int> &parent){
+        int xp = find(x,parent);
+        int yp = find(y,parent);
+        if(xp == yp){
+            return;
+        }
+        if(xp < yp){
+            parent[yp] = xp;
+        }
+        else if(xp>yp){
+            parent[xp] = yp;
+        }
+        else{
+            parent[xp] = yp;
+        }
     }
     string smallestEquivalentString(string s1, string s2, string baseStr) {
         int n = s1.length();
@@ -20,11 +32,25 @@ public:
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
+        vector<int> parent(26,0);
+        for(int i=0 ; i<26 ; i++){
+            parent[i] = i;
+        }
+        for(int i=0 ; i<n ; i++){
+            int x = s1[i];
+            int y = s2[i];
+            int xp = find(x-'a',parent);
+            int yp = find(y-'a',parent);
+            if(xp != yp){
+                unionf(x-'a',y-'a',parent);
+
+            }
+        }
         string result = "";
         for(int i=0 ; i<baseStr.size() ; i++){
-            vis.assign(26,false);
-            result += dfs(adj , baseStr[i] , vis);
+            result += char(find(baseStr[i]-'a',parent)+'a');
         }
         return result;
+
     }
 };
