4dcfd301f67fc3637b9f84c279b2f252a6dfcd44
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-03-30 17:13:12 +0530

    Time: 4 ms (85.71%), Space: 33.3 MB (85.17%) - LeetHub

--- 1738-maximal-network-rank/1738-maximal-network-rank.cpp
+++ 1738-maximal-network-rank/1738-maximal-network-rank.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        vector<unordered_set<int>> adj(n);
+        vector<int> indegree(n,0);
+        vector<vector<bool>> common(n,vector<bool>(n,false));
         for(auto &road : roads){
             int u = road[0];
             int v = road[1];
-            adj[u].insert(v);
-            adj[v].insert(u);
+            indegree[u]++;
+            indegree[v]++;
+            common[u][v] = true;
+            common[v][u] = true;
         }
         int maxRank = 0;
         for(int i=0 ; i<n ; i++){
             for(int j=i+1 ; j<n ; j++){
-                int iRank = adj[i].size();
-                int jRank = adj[j].size();
-                int rank = iRank + jRank;
-                if(adj[j].find(i) != adj[j].end()){
-                    rank -= 1;
+                int totalRank = indegree[i] + indegree[j];
+                if(common[i][j]){
+                    totalRank -= 1;
                 }
-                maxRank = max(maxRank,rank);
+                maxRank = max(maxRank,totalRank);
             }
         }
         return maxRank;
