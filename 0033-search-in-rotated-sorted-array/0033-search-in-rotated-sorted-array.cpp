74d2fb208794c60354c7bd752518319b84166e2e
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-04-10 18:01:09 +0530

    Time: 0 ms (100%), Space: 15.1 MB (66.57%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    int binSearch(vector<int>& nums, int l, int h, int target) {
-        while (l <= h) {
-            int mid = (l + h) / 2;
-            if (nums[mid] == target) {
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        while(l <= r){
+            int mid = l+(r-l)/2;
+            if(nums[mid] == target){
                 return mid;
-            } else if (nums[l] <= nums[mid]) {
-                if (nums[l] <= target && target <= nums[mid]) {
-                    h = mid - 1;
-                } else {
+            }
+            else if(nums[l] <= nums[mid]){
+                if(nums[l] <= target && nums[mid] > target){
+                    r = mid - 1;
+                }
+                else{
                     l = mid + 1;
                 }
-            } else {
-                if (nums[mid] <= target && target <= nums[h]) {
+            }
+            else{
+                if(nums[mid] < target && target <= nums[r]){
                     l = mid + 1;
-                } else {
-                    h = mid - 1;
+                }
+                else{
+                    r = mid - 1;
                 }
             }
         }
         return -1;
-    }
-
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        return binSearch(nums, 0, n - 1, target);
     }
 };
