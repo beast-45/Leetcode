6490d54ae6d5852a3ce648629a926a8d082ee0ab
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-04-16 16:23:18 +0530

    Time: 12 ms (75.72%), Space: 75.4 MB (23.52%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
+    int m,n;
+    vector<vector<int>> directions{{-1,1},{0,1},{1,1}};
+    int dfs(int i , int j , int currentValue , vector<vector<int>> &grid , vector<vector<int>>&dp){
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j] <= currentValue){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int mMoves = 0;
+        for(auto &direction : directions){
+            int ni = i + direction[0];
+            int nj = j + direction[1];
+            int moves = dfs(ni,nj,grid[i][j],grid,dp);
+            mMoves = max(mMoves,moves);
+        }
+        return dp[i][j] = 1 + mMoves;
+    }
+
     int maxMoves(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        int dist[m][n];
-        memset(dist, 0, sizeof(dist));
-        int ans = 0;
-        queue<pair<int, int>> q;
-        for (int i = 0; i < m; ++i) {
-            q.emplace(i, 0);
+        m = grid.size();
+        n = grid[0].size();
+        int maxMoves = 0;
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        for(int i=0 ; i<m ; i++){
+            int moves = dfs(i,0,-1,grid,dp);
+            maxMoves = max(maxMoves,moves);
         }
-        int dirs[3][2] = { {-1, 1}, {0, 1}, {1, 1} };
-        while (!q.empty()) {
-            auto [i, j] = q.front();
-            q.pop();
-            for (int k = 0; k < 3; ++k) {
-                int x = i + dirs[k][0], y = j + dirs[k][1];
-                if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] > grid[i][j] && dist[x][y] < dist[i][j] + 1) {
-                    dist[x][y] = dist[i][j] + 1;
-                    ans = max(ans, dist[x][y]);
-                    q.emplace(x, y);
-                }
-            }
-        }
-        return ans;
+        return maxMoves-1;
     }
 };
