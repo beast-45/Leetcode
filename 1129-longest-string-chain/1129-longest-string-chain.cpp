8a39538d9bb870808bcd157f748079e291949a78
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-04-23 05:55:20 +0530

    Time: 155 ms (31.44%), Space: 105.9 MB (28.83%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,60 +1,43 @@
 class Solution {
 public:
-    static bool sortBasedOnLength(const string &word1 , const string &word2){
-        return word1.length() < word2.length();
-    }
-
     bool check(string &word1 , string &word2){
         int n1 = word1.length();
         int n2 = word2.length();
-
-        if(n1+1 != n2){
-            return false;
-        }
-
-        int i = 0; 
-        int j = 0;
-        int extraWord = 0;
-
+        if(n1+1 != n2) return false;
+        int i = 0 , j = 0 , extraWord = 0;
         while(i<n1 && j<n2){
             if(word1[i] == word2[j]){
                 i++;
                 j++;
             }
             else{
-                extraWord += 1;
-                if(extraWord > 1){
-                    return false;
-                }
+                extraWord++;
+                if(extraWord > 1) return false;
                 j++;
             }
         }
         return true;
     }
-    int solve(int index , int prevIndex , vector<string> &words , vector<vector<int>> &dp){
-        if(index >= words.size()){
+    int solve(int index , int prevIndex , int n , vector<string> &words , vector<vector<int>> &dp){
+        if(index >= n){
             return 0;
         }
-
-        if(prevIndex != -1 && dp[index][prevIndex] != -1){
-            return dp[index][prevIndex];
-        }
         int take = 0;
-        if(prevIndex == -1 || check(words[prevIndex] , words[index]) == true){
-            take = 1 + solve(index+1 , index , words , dp);
+        if(dp[index][prevIndex+1] != -1){
+            return dp[index][prevIndex+1];
         }
-        int skip = solve(index+1 , prevIndex , words , dp);
-
-        int ans = max(take , skip);
-        if(prevIndex != -1){
-            dp[index][prevIndex] = ans;
+        if(prevIndex == -1 || check(words[prevIndex],words[index])){
+            take = 1 + solve(index+1,index,n,words,dp);
         }
-        return ans;
+        int skip = solve(index+1,prevIndex,n,words,dp);
+        return dp[index][prevIndex+1] = max(take,skip);
     }
     int longestStrChain(vector<string>& words) {
         int n = words.size();
-        vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
-        sort(begin(words) , end(words) , sortBasedOnLength);
-        return solve(0 , -1 , words , dp);
+        sort(begin(words),end(words),[&](string &w1 , string &w2){
+            return w1.length() < w2.length();
+        });
+        vector<vector<int>> dp(n+2,vector<int>(n+2,-1));
+        return solve(0,-1,n,words,dp);
     }
 };
