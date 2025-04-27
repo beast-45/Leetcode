1a031b10298d3b05e7e17b433da99108d3d4df54
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-04-27 19:57:26 +0530

    Time: 49 ms (74.85%), Space: 18.4 MB (94.47%) - LeetHub

--- 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.cpp
+++ 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>> &adj , int u , vector<bool> &vis){
-        int bombs = 1;
-        vis[u] = true;
-        for(int &v : adj[u]){
-            if(!vis[v]){
-                bombs += dfs(adj , v , vis);
+    using LL = long long;
+    int dfs(int bomb , vector<bool> &vis , vector<vector<int>> &adj){
+        int dBombs = 1;
+        vis[bomb] = true;
+        for(int &nBomb : adj[bomb]){
+            if(!vis[nBomb]){
+                dBombs += dfs(nBomb,vis,adj);
             }
         }
-        return bombs;
+        return dBombs;
     }
     int maximumDetonation(vector<vector<int>>& bombs) {
         int n = bombs.size();
@@ -16,23 +17,20 @@ public:
         for(int i=0 ; i<n ; i++){
             for(int j=0 ; j<n ; j++){
                 if(i == j) continue;
-                long long x1 = bombs[i][0];
-                long long y1 = bombs[i][1];
-                long long r1 = bombs[i][2];
-                long long x2 = bombs[j][0];
-                long long y2 = bombs[j][1];
-                long long d = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1);
-                if(d<=r1*r1){
+                LL x1 = bombs[i][0] , y1 = bombs[i][1] , r1 = bombs[i][2];
+                LL x2 = bombs[j][0] , y2 = bombs[j][1];
+                LL d = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1);
+                if(d <= r1*r1){
                     adj[i].push_back(j);
-                } 
+                }
             }
         }
-        int maxBombs = 0;
+        vector<bool> vis(n,false);
+        int maxD = 0;
         for(int i=0 ; i<n ; i++){
-            vector<bool> vis(n,false);
-            int bombs = dfs(adj , i , vis);
-            maxBombs = max(maxBombs,bombs);
+            vis.assign(n,false);
+            maxD = max(maxD,dfs(i,vis,adj));
         }
-        return maxBombs;
+        return maxD;
     }
 };
