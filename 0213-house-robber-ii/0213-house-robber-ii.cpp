cdae14b8eaebc90cf3c2bd546ec6ee48bcd55adc
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-05-01 10:40:52 +0530

    Time: 0 ms (100%), Space: 10.9 MB (40.29%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    int t[101];
-    int solve(vector<int>& nums, int i, int n) {
-        if (i > n) {
+    int solve(int index , int n , vector<int> &nums , vector<int> &dp){
+        if(index >= n){
             return 0;
         }
-        if (t[i] != -1) {
-            return t[i];
+        if(dp[index] != -1){
+            return dp[index];
         }
-
-        int take = nums[i] + solve(nums, i + 2, n);
-        int skip = solve(nums, i + 1, n);
-        return t[i] = max(take, skip);
+        int steal = nums[index] + solve(index+2,n,nums,dp);
+        int skip = solve(index+1,n,nums,dp);
+        return dp[index] = max(steal,skip);
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if (n == 1)
-            return nums[0];
-        if (n == 2)
-            return max(nums[0], nums[1]);
-
-        memset(t, -1, sizeof(t));
-        int take_0th = solve(nums, 0, n - 2);
-
-        memset(t, -1, sizeof(t));
-        int take_1st = solve(nums, 1, n - 1);
-
-        return max(take_0th, take_1st);
+        if(n == 1) return nums[0];
+        if(n == 2){
+            return max(nums[0],nums[1]);
+        }
+        vector<int> dp(n+1,-1);
+        int startFromFirst = solve(0,n-1,nums,dp);
+        dp.assign(n,-1);
+        int startFromSecond = solve(1,n,nums,dp);
+        return max(startFromFirst,startFromSecond);
     }
 };
