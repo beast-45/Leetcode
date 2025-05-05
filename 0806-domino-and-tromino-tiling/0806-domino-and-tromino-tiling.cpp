40b3aeada0111517ee662b57ad394be8296b75c7
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-05-05 14:05:07 +0530

    Time: 0 ms (100%), Space: 7.7 MB (94.11%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
@@ -3,13 +3,12 @@ public:
     int M = 1000000007;
     int t[1001];
     int solve(int n) {
-         if(n == 1 || n == 2) return n;
+        if(n == 1 || n == 2) return n;
         if(n == 3) return 5;
         
         if(t[n] != -1) return t[n];
         return t[n] = (2*solve(n-1)%M + solve(n-3)%M)%M;
     }
-    
     int numTilings(int n) {
        memset(t, -1, sizeof(t));
        return solve(n);  
