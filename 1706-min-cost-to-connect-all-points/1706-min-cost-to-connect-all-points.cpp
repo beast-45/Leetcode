bb117a06daad4230240e9ef9048d9924d73d288f
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-05-10 15:06:44 +0530

    Time: 449 ms (40.85%), Space: 134.8 MB (33.39%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    typedef pair<int,int> P; 
-    int minCostConnectPoints(vector<vector<int>>& points) {
-        int n = points.size();
-        vector<vector<pair<int,int>>> adj(n);
-        for(int i=0 ; i<n-1 ; i++){
-            for(int j=i+1 ; j<n ; j++){
-                int  w = abs(points[i][0]-points[j][0]) + abs(points[i][1]-points[j][1]);
-                adj[i].push_back({j,w});
-                adj[j].push_back({i,w});
-            }
-        }
-        int mstSUM = 0;
-        vector<bool> vis(n,false);
+    using P = pair<int,int>;
+    int primsAlgo(int n , vector<vector<P>> &adj){
+        int mstSum = 0;
+        vector<bool> vis(n,false);   // already in MST
         priority_queue<P,vector<P>,greater<P>> pq;
         pq.push({0,0});
         while(!pq.empty()){
-            auto u = pq.top();
+            auto [cost,node] = pq.top();
             pq.pop();
-            int w = u.first;
-            int node = u.second;
-            if(vis[node]){
-                continue;
-            }
+            if(vis[node]) continue;
+            mstSum += cost;
             vis[node] = true;
-            mstSUM += w;
-            for(auto &neighbor : adj[node]){
-                int nNode = neighbor.first;
-                int wt = neighbor.second;
-                if(!vis[nNode]){
-                    pq.push({wt,nNode});
-                }
+            for(auto &[nNode,nCost] : adj[node]){
+                if(!vis[nNode]) pq.push({nCost,nNode});
             }
         }
-        return mstSUM;
+        return mstSum;
+    }
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        vector<vector<P>> adj(n);
+        for(int i=0 ; i<n ; i++){
+            for(int j=i+1 ; j<n ; j++){
+                int d = abs(points[j][0]-points[i][0])+abs(points[j][1]-points[i][1]);
+                adj[i].push_back({j,d});
+                adj[j].push_back({i,d});
+            }
+        }
+        return primsAlgo(n,adj);
     }
 };
