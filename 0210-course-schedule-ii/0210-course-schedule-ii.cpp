58a650e605405c7ff30102462767677dd93137b2
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-05-11 16:46:01 +0530

    Time: 0 ms (100%), Space: 18.2 MB (61.39%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,43 +1,36 @@
 class Solution {
 public:
-    bool hasCycle = false;
-    void dfs(vector<vector<int>> &adj , int u , vector<bool> &vis , vector<bool> &inRec , vector<int> &result){
-        vis[u] = true;
-        inRec[u] = true;
-        for(int &v : adj[u]){
-            if(!vis[v]){
-                dfs(adj , v , vis , inRec , result);
-            }
-            else if(inRec[v]){
-                hasCycle = true;
-                return;
+    void kahnAlgo(int n , int &count , vector<int> &indegree , vector<int> &result , vector<vector<int>> &adj){
+        queue<int> q;
+        for(int i=0 ; i<n ; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+                count += 1;
             }
         }
-        inRec[u] = false;
-        result.push_back(u);
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            result.push_back(u);
+            for(auto &v : adj[u]){
+                indegree[v]--;
+                if(indegree[v] == 0){
+                    q.push(v);
+                    count += 1;
+                }
+            }
+        }
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         int n = numCourses;
-        vector<bool> vis(n,false);
-        vector<bool> inRec(n,false);
-        vector<int> result;
+        vector<int> indegree(n) , result;
         vector<vector<int>> adj(n);
-        for(vector<int> &pre : prerequisites){
-            int a = pre[0];
-            int b = pre[1];
-            adj[b].push_back(a);
+        for(auto &pre : prerequisites){
+            adj[pre[1]].push_back(pre[0]);
+            indegree[pre[0]]++;
         }
-        for(int i=0 ; i<n ; i++){
-            if(!vis[i]){
-                dfs(adj , i , vis , inRec , result);
-            }
-        }
-        if(hasCycle){
-            return {};
-        }
-        else{
-            reverse(begin(result),end(result));
-        }
-        return result;
+        int count = 0;
+        kahnAlgo(n,count,indegree,result,adj);
+        return count == n ? result : vector<int>{};
     }
 };
