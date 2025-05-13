90646c42f6c928a4dea4198009b73d3c6422187e
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-05-13 10:28:05 +0530

    Time: 0 ms (100%), Space: 9.3 MB (49.88%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,52 +1,36 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<int> spiOrder;
-
-        int id = 0;
-        int top = 0;
-        int down = m-1;
-        int left = 0;
-        int right = n-1;
-
-        while(top <= down && left <= right)
-        {
-            if(id == 0)
-            {
-                for(int i=left ; i<=right ; i++)
-                {
-                    spiOrder.push_back(matrix[top][i]);
+        int rows = matrix.size() , cols = matrix[0].size();
+        int top = 0 , down = rows-1 , left = 0 , right = cols-1 , direction = 0;
+        vector<int> result;
+        while(left <= right && top <= down){
+            if(direction == 0){
+                for(int i=top ; i<=right ; i++){
+                    result.push_back(matrix[top][i]);
                 }
                 top++;
             }
-            if(id == 1)
-            {
-                for(int i=top ; i<=down ; i++)
-                {
-                    spiOrder.push_back(matrix[i][right]);
+            if(direction == 1){
+                for(int i=top ; i<=down ; i++){
+                    result.push_back(matrix[i][right]);
                 }
                 right--;
             }
-            if(id == 2)
-            {
-                for(int i=right ; i>=left ; i--)
-                {
-                    spiOrder.push_back(matrix[down][i]);
+            if(direction == 2){
+                for(int i=right ; i>=left ; i--){
+                    result.push_back(matrix[down][i]);
                 }
                 down--;
             }
-            if(id == 3)
-            {
-                for(int i=down ; i>=top ; i--)
-                {
-                    spiOrder.push_back(matrix[i][left]);
+            if(direction == 3){
+                for(int i=down ; i>=top ; i--){
+                    result.push_back(matrix[i][left]);
                 }
                 left++;
             }
-            id = (id+1)%4;
+            direction = (direction+1)%4;
         }
-        return spiOrder;
+        return result;
     }
 };
