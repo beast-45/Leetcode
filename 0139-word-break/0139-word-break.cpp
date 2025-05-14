a0966707ff050d1091d175b5418053426441caa3
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-05-14 10:52:58 +0530

    Time: 15 ms (31.56%), Space: 17 MB (28.27%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    bool solve(int index , int n , string &s , unordered_set<string> &wDict , vector<int> &dp){
-        if(index >= n){
-            return true;
-        }
+    bool solve(int index , int n , string &s , unordered_set<string> &st , vector<int> &dp){
+        if(index >= n) return true;
         if(dp[index] != -1){
             return dp[index];
         }
-        for(int len=1 ; len<=n ; len++){
+        for(int len=1 ; index+len<=n ; len++){
             string temp = s.substr(index,len);
-            if(wDict.find(temp) != wDict.end() && solve(index+len,n,s,wDict,dp)){
+            if(st.find(temp) != st.end() && solve(index+len,n,s,st,dp)){
                 return dp[index] = true;
             }
         }
         return dp[index] = false;
     }
-    bool wordBreak(string s, vector<string>& wordDict) { 
+    bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.length();
+        unordered_set<string> st(begin(wordDict),end(wordDict));
         vector<int> dp(n+1,-1);
-        unordered_set<string> wDict(begin(wordDict),end(wordDict));
-        return solve(0,n,s,wDict,dp);
+        return solve(0,n,s,st,dp);
     }
 };
