15856f13dc57dafe4eea3fdb73c9d5140bac6f91
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-05-16 12:26:32 +0530

    Time: 0 ms (100%), Space: 10.6 MB (37.19%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<string>> result;
-    bool check(int row , int col , int n , vector<string> &board){
+    bool check(vector<string> &board , int row , int col , int n){
         for(int r=row-1 ; r>=0 ; r--){
             if(board[r][col] == 'Q'){
                 return false;
@@ -9,36 +9,36 @@ public:
         }
         int c = col;
         for(int r=row-1 ; r>=0 ; r--){
-            c--;
-            if(c>=0 && board[r][c] == 'Q'){
+            c += 1;
+            if(c<n && board[r][c] == 'Q'){
                 return false;
             }
         }
         c = col;
         for(int r=row-1 ; r>=0 ; r--){
-            c++;
-            if(c<n && board[r][c] == 'Q'){
+            c -= 1;
+            if(c>=0 && board[r][c] == 'Q'){
                 return false;
             }
         }
         return true;
     }
-    void solve(int row , int n , vector<string> &board){
+    void solve(vector<string> &board , int row , int n){
         if(row >= n){
             result.push_back(board);
             return;
         }
         for(int col=0 ; col<n ; col++){
-            if(check(row,col,n,board)){
+            if(check(board,row,col,n)){
                 board[row][col] = 'Q';
-                solve(row+1,n,board);
+                solve(board,row+1,n);
                 board[row][col] = '.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<string> board(n,string(n,'.'));
-        solve(0,n,board);
+        solve(board,0,n);
         return result;
     }
 };
