48618b540c8cc3c6374ecab797df11c4a9353522
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-05-31 08:07:06 +0530

    Time: 4 ms (97.59%), Space: 102.3 MB (69.29%) - LeetHub

--- 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    void dfs(vector<int> &edges , int node , vector<int> &result , vector<bool> &vis){
+    void dfs(int node , vector<int> &result , vector<int> &edges, vector<bool> &vis){
         vis[node] = true;
         int nextNode = edges[node];
         if(nextNode != -1 && !vis[nextNode]){
             result[nextNode] = result[node]+1;
-            dfs(edges,nextNode,result,vis);
+            dfs(nextNode,result,edges,vis);
         }
     }
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
         int n = edges.size();
-
         vector<bool> vis(n,false);
-        vector<int> result1(n,INT_MAX);
+        vector<int> result1(n,INT_MAX) , result2(n,INT_MAX);
         result1[node1] = 0;
-        dfs(edges,node1,result1,vis);
-
-        fill(begin(vis),end(vis),false);
-        vector<int> result2(n,INT_MAX);
         result2[node2] = 0;
-        dfs(edges,node2,result2,vis);
+        dfs(node1,result1,edges,vis);
+        vis.assign(n,false);
+        dfs(node2,result2,edges,vis);
 
-        int node = -1;
-        int minDist = INT_MAX;
-
+        int node = -1 , minDist = INT_MAX;
         for(int i=0 ; i<n ; i++){
             int maxDist = max(result1[i],result2[i]);
             if(maxDist < minDist){
-                minDist = maxDist;
                 node = i;
+                minDist = maxDist;
             }
         }
-        return node;  
+        return node;
     }
 };
