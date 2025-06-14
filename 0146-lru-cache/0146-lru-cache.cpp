05b689e786afbbc7b2a5eb52219a86a361353c77
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-06-14 11:13:49 +0530

    Time: 114 ms (21.86%), Space: 182.2 MB (36.9%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,34 +1,34 @@
 class LRUCache {
 public:
     list<int> dll;
-    map<int,pair<list<int>::iterator,int>> mp;
+    unordered_map<int,pair<list<int>::iterator,int>> mp;
     int n;
     LRUCache(int capacity) {
         n = capacity;
     }
-    void recentlyUsed(int key){
+    void makeMostRecentlyUsed(int key){
         dll.erase(mp[key].first);
         dll.push_front(key);
-        mp[key].first = dll.begin(); 
+        mp[key].first = dll.begin();
     }
     int get(int key) {
         if(mp.find(key) == mp.end()) return -1;
-        recentlyUsed(key);
+        makeMostRecentlyUsed(key);
         return mp[key].second;
     }
     
     void put(int key, int value) {
         if(mp.find(key) != mp.end()){
             mp[key].second = value;
-            recentlyUsed(key);
+            makeMostRecentlyUsed(key);
         }else{
             dll.push_front(key);
             mp[key] = {dll.begin(),value};
             n--;
         }
         if(n<0){
-            int delKey = dll.back();
-            mp.erase(delKey);
+            int keyToBeDeleted = dll.back();
+            mp.erase(keyToBeDeleted);
             dll.pop_back();
             n++;
         }
