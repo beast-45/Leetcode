5a898fbe383de335281ea8fb733df098d5c9dc57
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-06-18 20:05:57 +0530

    Time: 0 ms (100%), Space: 17.5 MB (52.73%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,46 +1,34 @@
 class Solution {
 public:
-    void leftIndex(vector<int> &nums , int target , vector<int> &result){
-        int l = 0;
-        int r = nums.size()-1;
+    void firstPosition(vector<int> &nums , int target , vector<int> &result){
+        int l = 0 , r = nums.size()-1;
         while(l<=r){
-            int mid =  l + (r - l) / 2;
+            int mid =  l+(r-l)/2;
             if(nums[mid] == target){
                 result[0] = mid;
                 r = mid - 1;
             }
-            else if(nums[mid] > target){
-                r = mid - 1;
-            }
-            else{
-                l = mid + 1;
-            }
+            else if(nums[mid] > target) r = mid-1;
+            else l = mid+1;
         }
     }
-    void rightIndex(vector<int> &nums , int target , vector<int> &result){
-        int l = 0;
-        int r = nums.size() - 1;
+    void lastPosition(vector<int> &nums , int target , vector<int> &result){
+        int l = 0 , r = nums.size() - 1;
         while(l<=r){
-            int mid = l + (r - l) / 2;
+            int mid = l+(r-l)/2;
             if(nums[mid] ==  target){
                 result[1] = mid;
                 l = mid + 1;
             }
-            else if(nums[mid] > target){
-                r = mid - 1;
-            }
-            else{
-                l = mid + 1;
-            }
+            else if(nums[mid] > target) r = mid-1;
+            else l = mid+1;
         }
     }
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> result(2,-1);
-        if(nums.empty()){
-            return result;
-        }
-        leftIndex(nums,target,result);
-        rightIndex(nums,target,result);
+        if(nums.empty()) return result;
+        firstPosition(nums,target,result);
+        lastPosition(nums,target,result);
         return result; 
     }
 };
