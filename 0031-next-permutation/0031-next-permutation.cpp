adfc2a21345af72dec026cf61524f0bc14bafed3
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-06-20 03:16:00 +0530

    Time: 0 ms (100%), Space: 15.7 MB (32.97%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void reverse(int l , int r , vector<int> &nums){
+        while(l <= r){
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+    }
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int swapIndex = -1;
+        for(int i=n-1 ; i>0 ; i--){
+            if(nums[i] > nums[i-1]){
+                swapIndex = i-1;
+                break;
+            }
+        }
+        if(swapIndex != -1){
+            int nextIndex = swapIndex;
+            for(int i=n-1 ; i>swapIndex ; i--){
+                if(nums[i] > nums[swapIndex]){
+                    nextIndex = i;
+                    break;
+                }
+            }
+            swap(nums[swapIndex],nums[nextIndex]);
+        }
+        reverse(swapIndex+1,n-1,nums);
+    }
+};
