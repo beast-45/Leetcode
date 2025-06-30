628e18533a8756859e0a41d1d366732650b7ce49
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-06-30 09:02:12 +0530

    Time: 60 ms (60.51%), Space: 182.4 MB (25.9%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -2,36 +2,30 @@ class NumArray {
 public:
     int n;
     vector<int> segTree;
-
-    void buildTree(int i , int l , int r , vector<int> &nums){
+    void buildSegTree(int i , int l , int r , vector<int> &nums){
         if(l == r){
             segTree[i] = nums[l];
             return;
         }
         int mid = l+(r-l)/2;
-        buildTree(2*i+1,l,mid,nums);
-        buildTree(2*i+2,mid+1,r,nums);
+        buildSegTree(2*i+1,l,mid,nums);
+        buildSegTree(2*i+2,mid+1,r,nums);
         segTree[i] = segTree[2*i+1] + segTree[2*i+2];
     }
-
     void updateSegTree(int index , int value , int i , int l , int r){
         if(l == r){
             segTree[i] = value;
             return;
         }
         int mid = l+(r-l)/2;
-        if(index <= mid){
-            updateSegTree(index,value,2*i+1,l,mid);
-        }else{
-            updateSegTree(index,value,2*i+2,mid+1,r);
-        }
+        index <= mid ? updateSegTree(index,value,2*i+1,l,mid) : updateSegTree(index,value,2*i+2,mid+1,r);
         segTree[i] = segTree[2*i+1] + segTree[2*i+2];
     }
     int querySegTree(int start , int end , int i , int l , int r){
-        if(l > end || r < start){
+        if(l>end || r<start){
             return 0;
         }
-        if(l >= start && r <= end){
+        if(l>=start && r<=end){
             return segTree[i];
         }
         int mid = l+(r-l)/2;
@@ -40,21 +34,15 @@ public:
     NumArray(vector<int>& nums) {
         n = nums.size();
         segTree.resize(4*n);
-        buildTree(0,0,n-1,nums);
+        buildSegTree(0,0,n-1,nums);
     }
     
     void update(int index, int val) {
-        if(index >= 0 && index < n){
-            updateSegTree(index,val,0,0,n-1);
-        }
+        updateSegTree(index,val,0,0,n-1);
     }
     
     int sumRange(int left, int right) {
-        if(left >= 0 && right < n){
-            return querySegTree(left,right,0,0,n-1);
-        }else{
-            return 0;
-        }
+        return querySegTree(left,right,0,0,n-1);
     }
 };
 
