75742de6928b9eca93d6848fb3ad05b57f598858
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-07-03 15:00:52 +0530

    Time: 782 ms (35.34%), Space: 266.3 MB (70.5%) - LeetHub

--- 3181-find-building-where-alice-and-bob-can-meet/3181-find-building-where-alice-and-bob-can-meet.cpp
+++ 3181-find-building-where-alice-and-bob-can-meet/3181-find-building-where-alice-and-bob-can-meet.cpp
@@ -1,66 +1,70 @@
-class BinaryIndexedTree {
-private:
-    int inf = 1 << 30;
-    int n;
-    vector<int> c;
-
-public:
-    BinaryIndexedTree(int n) {
-        this->n = n;
-        c.resize(n + 1, inf);
-    }
-
-    void update(int x, int v) {
-        while (x <= n) {
-            c[x] = min(c[x], v);
-            x += x & -x;
-        }
-    }
-
-    int query(int x) {
-        int mi = inf;
-        while (x > 0) {
-            mi = min(mi, c[x]);
-            x -= x & -x;
-        }
-        return mi == inf ? -1 : mi;
-    }
-};
-
 class Solution {
 public:
+    int n;
+    vector<int> segmentTree;
+    void buildSegmentTree(int i , int l , int r , vector<int> &nums){
+        if(l == r){
+            segmentTree[i] = l;
+            return;
+        }
+        int mid = l+(r-l)/2;
+        buildSegmentTree(2*i+1,l,mid,nums);
+        buildSegmentTree(2*i+2,mid+1,r,nums);
+        int leftMaxIndex = segmentTree[2*i+1] , rightMaxIndex = segmentTree[2*i+2];
+        segmentTree[i] = (nums[leftMaxIndex] >= nums[rightMaxIndex]) ? leftMaxIndex : rightMaxIndex;
+    }
+    int querySegmentTree(int start , int end , int i , int l , int r , vector<int> &heights){
+        if(l>end || r<start){
+            return -1;
+        }
+        if(l>=start && r<=end){
+            return segmentTree[i]; 
+        }
+        int mid = l+(r-l)/2;
+        int leftIndex = querySegmentTree(start,end,2*i+1,l,mid,heights);
+        int rightIndex = querySegmentTree(start,end,2*i+2,mid+1,r,heights);
+        if(leftIndex == -1) return rightIndex;
+        if(rightIndex == -1) return leftIndex;
+        return heights[leftIndex] >= heights[rightIndex] ? leftIndex : rightIndex;
+    }
+    int rangeMIQ(int start , int end , vector<int> &heights){
+        return querySegmentTree(start,end,0,0,n-1,heights);
+    }
     vector<int> leftmostBuildingQueries(vector<int>& heights, vector<vector<int>>& queries) {
-        int n = heights.size(), m = queries.size();
-        for (auto& q : queries) {
-            if (q[0] > q[1]) {
-                swap(q[0], q[1]);
+        n = heights.size();
+        segmentTree.resize(4*n);
+        buildSegmentTree(0,0,n-1,heights);
+
+        vector<int> result;
+        for(auto &query : queries){
+            int start = query[0] , end = query[1];
+            if(start == end){
+                result.push_back(start);
+                continue;
             }
+            if(start < end && heights[start] < heights[end]){
+                result.push_back(end);
+                continue;
+            }
+            if(end < start && heights[end] < heights[start]){
+                result.push_back(start);
+                continue;
+            }
+            int maxIndex = max(start,end);
+            int l = maxIndex+1 , r = n-1;
+            int resultIndex = -1;
+            while(l <= r){
+                int mid = l+(r-l)/2;
+                int index = rangeMIQ(l,mid,heights);
+                if(index != -1 &&  heights[index] > max(heights[start], heights[end])){
+                    resultIndex = index;
+                    r = mid-1;
+                }else{
+                    l = mid+1;
+                }
+            }
+            result.push_back(resultIndex);
         }
-        vector<int> idx(m);
-        iota(idx.begin(), idx.end(), 0);
-        sort(idx.begin(), idx.end(), [&](int i, int j) {
-            return queries[j][1] < queries[i][1];
-        });
-        vector<int> s = heights;
-        sort(s.begin(), s.end());
-        s.erase(unique(s.begin(), s.end()), s.end());
-        vector<int> ans(m);
-        int j = n - 1;
-        BinaryIndexedTree tree(n);
-        for (int i : idx) {
-            int l = queries[i][0], r = queries[i][1];
-            while (j > r) {
-                int k = s.end() - lower_bound(s.begin(), s.end(), heights[j]) + 1;
-                tree.update(k, j);
-                --j;
-            }
-            if (l == r || heights[l] < heights[r]) {
-                ans[i] = r;
-            } else {
-                int k = s.end() - lower_bound(s.begin(), s.end(), heights[l]);
-                ans[i] = tree.query(k);
-            }
-        }
-        return ans;
+        return result;
     }
 };
