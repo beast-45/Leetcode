bbbca007482f0dee0f7c3d2a8584d5cd84ef5c68
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-07-08 11:31:42 +0530

    Time: 285 ms (59.16%), Space: 10.7 MB (62.03%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     int m,n;
-    vector<vector<bool>> vis;
-    vector<vector<int>> directions{{1,0},{-1,0},{0,1},{0,-1}};
-    bool dfs(int i , int j , int index , string &word , vector<vector<char>> &board){
-        if(index == word.length()){
-            return true;
-        }
+    vector<pair<int,int>> directions {{1,0},{-1,0},{0,-1},{0,1}};
+    bool dfs(int i , int j , int index , string &word , vector<vector<char>> &board , vector<vector<bool>> &vis){
+        if(index == word.length()) return true;
         if(i<0 || i>=m || j<0 || j>=n || board[i][j] != word[index] || vis[i][j]){
             return false;
         }
         vis[i][j] = true;
-        for(auto &direction : directions){
-            int ni = i + direction[0];
-            int nj = j + direction[1];
-            if(dfs(ni,nj,index+1,word,board)){
-                return true;
-            }
+        for(auto &[di,dj] : directions){
+            int ni = i + di , nj = j + dj;
+            if(dfs(ni,nj,index+1,word,board,vis)) return true;
         }
         vis[i][j] = false;
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        m = board.size();
-        n = board[0].size();
-        vis.resize(m,vector<bool>(n,false));
+        m = board.size() , n = board[0].size();
+        vector<vector<bool>> vis(m,vector<bool>(n,false));
         for(int i=0 ; i<m ; i++){
             for(int j=0 ; j<n ; j++){
                 if(board[i][j] == word[0]){
-                    if(dfs(i,j,0,word,board)){
-                        return true;
-                    }
+                    if(dfs(i,j,0,word,board,vis)) return true;
                 }
             }
         }
