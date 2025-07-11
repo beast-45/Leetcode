1e3dee8f69f8b72a6798fe7f26aee33110ec1841
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-07-11 14:26:54 +0530

    Time: 129 ms (86.91%), Space: 160.7 MB (52.63%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,33 +1,48 @@
 class Solution {
 public:
-    int countDigits(int n){
-        int count = 0;
-        while(n){
-            count += 1;
-            n /= 10;
+    struct trieNode{
+        trieNode* child[10];
+    };
+    trieNode* makeTrieNode(){
+        trieNode* nNode = new trieNode;
+        for(int i=0 ; i<10 ; i++){
+            nNode->child[i] = nullptr;
         }
-        return count;
+        return nNode;
+    }
+    void insert(trieNode* root , int num){
+        string word = to_string(num);
+        trieNode* crawler = root;
+        for(char &ch : word){
+            if(!crawler->child[ch-'0']){
+                crawler->child[ch-'0'] = makeTrieNode();
+            }
+            crawler = crawler->child[ch-'0'];
+        }
+    }
+    int search(trieNode* root , int num){
+        string word = to_string(num);
+        trieNode* crawler = root;
+        int length = 0;
+        for(char &ch : word){
+            if(crawler->child[ch-'0']){
+                length += 1;
+                crawler = crawler->child[ch-'0'];
+            }else{
+                break;
+            }
+        }
+        return length;
     }
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        unordered_map<int,int> mp;
-        for(int &num : arr1){
-            mp[num] = countDigits(num);
-            while(num){
-                num /= 10;
-                mp[num] = countDigits(num);
-            }
+        trieNode* root = makeTrieNode();
+        for(int num : arr1){
+            insert(root,num);
         }
         int maxLen = 0;
-        for(int &num : arr2){
-            if(mp.find(num) != mp.end()){
-                maxLen = max(maxLen,mp[num]);
-            }
-            while(num){
-                num /= 10;
-                if(mp.find(num) != mp.end()){
-                    maxLen = max(maxLen,mp[num]);
-                }
-            }
+        for(int num : arr2){
+            int len = search(root,num);
+            maxLen = max(maxLen,len);
         }
         return maxLen;
     }
