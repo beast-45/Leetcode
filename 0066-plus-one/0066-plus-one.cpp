d70e74320e8115f3dae47242a724048e84cdfe47
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-07-24 11:01:15 +0530

    Time: 0 ms (100%), Space: 11.5 MB (34.65%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        int carry = 1;
+        for(int i=n-1 ; i>=0 ; i--){
+            int sum = digits[i] + carry;
+            carry = sum / 10;
+            digits[i] = sum % 10;
+        }
+        if(carry != 0){
+            digits.insert(digits.begin(),carry);
+        }
+        return digits;
+    }
+};
