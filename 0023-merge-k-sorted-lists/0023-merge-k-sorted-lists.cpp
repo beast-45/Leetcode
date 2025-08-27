9eedbe1b4588f7067e356ac5c4a6edd657a72455
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-08-27 18:08:23 +0530

    Time: 0 ms (100%), Space: 18.6 MB (50.91%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,37 +10,30 @@
  */
 class Solution {
 public:
-    ListNode* merge2Lists(ListNode *L1 , ListNode* L2){
+    ListNode* mergeTwoLL(ListNode* L1 , ListNode* L2){
         if(!L1) return L2;
         if(!L2) return L1;
         ListNode* result;
-        if(L1->val < L2->val){
+        if(L1->val <= L2->val){
             result = L1;
-            result->next = merge2Lists(L1->next,L2);
-        }
-        else{
+            result->next = mergeTwoLL(L1->next,L2);
+        }else{
             result = L2;
-            result->next = merge2Lists(L1,L2->next);
+            result->next = mergeTwoLL(L1,L2->next);
         }
         return result;
     }
-    ListNode* provide2Lists(int start , int end , vector<ListNode*> &lists){
-        if(start > end){
-            return NULL;
-        }
-        if(start == end){
-            return lists[start];
-        }
-        int mid = start + (end - start) / 2;
-        ListNode* L1 = provide2Lists(start , mid , lists);
-        ListNode* L2 = provide2Lists(mid+1 , end , lists);
-        return merge2Lists(L1,L2);
+    ListNode* provideTwoLL(int start , int end , vector<ListNode*> &lists){
+        if(start > end) return NULL;
+        if(start == end) return lists[start];
+        int mid =  start+(end-start)/2;
+        ListNode* L1 = provideTwoLL(start,mid,lists);
+        ListNode* L2 = provideTwoLL(mid+1,end,lists);
+        return mergeTwoLL(L1,L2);
     }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         int n = lists.size();
-        if(n == 0){
-            return NULL;
-        }
-        return provide2Lists(0 , n-1 , lists);
+        if(n == 0) return NULL;
+        return provideTwoLL(0,n-1,lists);
     }
 };
