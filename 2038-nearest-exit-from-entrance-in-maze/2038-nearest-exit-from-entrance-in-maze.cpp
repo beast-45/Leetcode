0ca7e688aad9b418bfbf479b6d341e8c2332915d
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-09-28 21:35:07 +0530

    Time: 3 ms (88.35%), Space: 35.9 MB (65.27%) - LeetHub

--- 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        int m = maze.size();
-        int n = maze[0].size();
-        vector<vector<int>> directions{{1,0},{-1,0},{0,-1},{0,1}};
-        queue<pair<int,int>> q;
-        vector<vector<bool>> vis(m,vector<bool>(n,false));
-        int entranceRow = entrance[0];
-        int entranceCol = entrance[1];
-        vis[entranceRow][entranceCol] = true;
-        q.push({entranceRow,entranceCol});
-        int level = 0;
-        while(!q.empty()){
+        int m = maze.size(), n = maze[0].size();
+        queue<pair<int, int>> q;
+        vector<pair<int, int>> directions{{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+        q.push({entrance[0], entrance[1]});
+        vis[entrance[0]][entrance[1]] = true;
+        int steps = 0;
+        while (!q.empty()){
             int s = q.size();
-            while(s--){
-                int i = q.front().first;
-                int j = q.front().second;
+            while (s--) {
+                int i = q.front().first, j = q.front().second;
                 q.pop();
-                for(vector<int>&direction : directions){
-                    int ni = i + direction[0];
-                    int nj = j + direction[1];
-                    if(ni>=0 && ni<m && nj>=0 && nj<n && maze[ni][nj] == '.' && !vis[ni][nj]){
-                        if(ni == m-1 || ni == 0 || nj==0 || nj==n-1){
-                            return level+1;
+                for (auto& [di, dj] : directions){
+                    int ni = i + di, nj = j + dj;
+                    if (ni >= 0 && ni < m && nj >= 0 && nj < n && maze[ni][nj] == '.' && !vis[ni][nj]) {
+                        if (ni == 0 || ni == m - 1 || nj == 0 || nj == n - 1){
+                            return steps + 1;
                         }
-                        q.push({ni,nj});
                         vis[ni][nj] = true;
-
+                        q.push({ni, nj});
                     }
                 }
             }
-            level++;
+            steps += 1;
         }
         return -1;
     }
