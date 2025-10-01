44fece6f4a7b1c3dbc5c66327ecae174c63eedf7
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-10-01 21:13:21 +0530

    Time: 2 ms (60.46%), Space: 13.8 MB (99.35%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    void solve(int index , int sum , vector<int> &temp , vector<vector<int>> &result , vector<int> &nums , int target){
-        if(sum == target){
+    void solve(int index , int target , int n ,  vector<int> &temp , vector<vector<int>> &result , vector<int> &nums){
+        if(target == 0){
             result.push_back(temp);
             return;
         }
-        if(index >= nums.size() || sum > target){
-            return;
-        }
+        if(index >= n || target < 0) return;
         temp.push_back(nums[index]);
-        solve(index,sum+nums[index],temp,result,nums,target);
+        solve(index,target-nums[index],n,temp,result,nums);
         temp.pop_back();
-        solve(index+1,sum,temp,result,nums,target);
+        solve(index+1,target,n,temp,result,nums);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int n = candidates.size();
         vector<int> temp;
         vector<vector<int>> result;
-        sort(begin(candidates),end(candidates));
-        solve(0,0,temp,result,candidates,target);
+        solve(0,target,n,temp,result,candidates);
         return result;
     }
 };
