04c5e7dab5a974928578cdb8b7ab41a06572a267
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-10-10 23:56:26 +0530

    Time: 0 ms (100%), Space: 18 MB (89.84%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    void dfs(int i , int j , int m , int n , int iColor , int color , vector<vector<int>> &image , vector<vector<int>> &directions){
+    void dfs(int i , int j , int m , int n , int iColor , int color , vector<vector<int>> &image , vector<pair<int,int>> &directions){
         if(i<0 || i>=m || j<0 || j>=n || image[i][j] != iColor){
             return;
         }
         image[i][j] = color;
-        for(vector<int> &direction : directions){
-            int ni = i + direction[0];
-            int nj = j + direction[1];
+        for(auto &[di,dj] : directions){
+            int ni = i + di;
+            int nj = j + dj;
             dfs(ni,nj,m,n,iColor,color,image,directions);
         }
     }
@@ -18,7 +18,7 @@ public:
         if(iColor == color){
             return image;
         }
-        vector<vector<int>> directions {{1,0},{-1,0},{0,1},{0,-1}};
+        vector<pair<int,int>> directions {{1,0},{-1,0},{0,1},{0,-1}};
         dfs(sr,sc,m,n,iColor,color,image,directions);
         return image;
     }
