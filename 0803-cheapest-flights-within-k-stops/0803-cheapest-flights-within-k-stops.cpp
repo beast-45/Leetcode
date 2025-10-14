829d0a7414140ff6be0f65769e746322ec98e9fc
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-10-14 18:28:06 +0530

    Time: 4 ms (46.2%), Space: 17.8 MB (76.32%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
+    using P = pair<int,int>;
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int> result(n,INT_MAX);
-        vector<vector<pair<int,int>>> adj(n);
+        vector<vector<P>> adj(n);
         for(auto &flight : flights){
-            int u = flight[0];
-            int v = flight[1];
-            int w = flight[2];
-            adj[u].push_back({v,w});
+            adj[flight[0]].push_back({flight[1],flight[2]});
         }
-        queue<pair<int,int>> q;
+        vector<int> result(n,INT_MAX);
+        queue<P> q;
         q.push({src,0});
         result[src] = 0;
         int stops = 0;
         while(!q.empty() && stops <= k){
             int s = q.size();
             while(s--){
-                int node = q.front().first;
-                int cost = q.front().second;
+                auto [node,cost] = q.front();
                 q.pop();
-                for(auto &neighbor : adj[node]){
-                    int nNode = neighbor.first;
-                    int nCost = neighbor.second;
+                for(auto &[nNode,nCost] : adj[node]){
                     if(cost + nCost < result[nNode]){
                         result[nNode] = cost + nCost;
                         q.push({nNode,result[nNode]});
