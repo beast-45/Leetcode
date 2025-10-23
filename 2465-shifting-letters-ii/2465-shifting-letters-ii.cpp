e8ce1820ab6b7c1057f412ffd320273ff5a35d60
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-10-23 11:11:16 +0530

    Time: 5 ms (65.91%), Space: 97.9 MB (54.24%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
-        int n = s.size();
-        vector<int> diff(n , 0); 
-
-        for (const auto& shift : shifts)
-        {
-            int start = shift[0];
-            int end = shift[1];
-            int direction = shift[2];
-
-            if (direction == 1)
-            { 
-                diff[start] += 1;
-                if(end+1 < n) diff[end + 1] -= 1;
-            }
-            else
-            { 
-                diff[start] -= 1;
-                if(end+1 < n) diff[end + 1] += 1;
+        int n = s.length();
+        vector<int> diff(n,0);
+        for(auto &shift : shifts){
+            int l = shift[0] , r = shift[1] , direction = shift[2];
+            if(direction == 1){
+                diff[l] += 1;
+                if(r+1 < n) diff[r+1] -= 1; 
+            }else{
+                diff[l] -= 1;
+                if(r+1 < n) diff[r+1] += 1;
             }
         }
-        
-        for (int i = 1; i < n; ++i)
-        {
-            diff[i] += diff[i - 1]; 
+        for(int i=1 ; i<n ; i++){
+            diff[i] += diff[i-1];
         }
-        
-        for (int i = 0; i < n; ++i)
-        {
-            int shift = diff[i] % 26; 
-            if (shift < 0) shift += 26; 
-            s[i] = ((s[i] - 'a' + shift) % 26) + 'a';
+        for(int i=0 ; i<n ; i++){
+            int shift = diff[i] % 26;
+            if(shift < 0) shift += 26;
+            s[i] = ((s[i]-'a' + shift)%26) + 'a';
         }
-        
-        return s;
+        return s;   
     }
 };
