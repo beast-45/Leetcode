32e0cb3c1303b514f4c423717fcd8dd8845398be
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-10-28 18:18:25 +0530

    Time: 77 ms (55.82%), Space: 76 MB (21.26%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    int t[1001][1001];
-
-    int LCS(string &s1 , string &s2 , int m , int n)
-    {
-        if(m == 0 || n == 0) return t[m][n] = 0;
-        if(t[m][n]  != -1) return t[m][n];
-
-        if(s1[m-1] == s2[n-1]) return t[m][n] = 1 + LCS(s1 , s2 , m-1 , n-1);
-
-        return t[m][n] = max(LCS(s1 , s2 , m , n-1) , LCS(s1 , s2 , m-1 , n));
+    int solve(int idx1 , int idx2 , int n , string &s1 , string &s2 , vector<vector<int>> &dp){
+        if(idx1 >= n || idx2 >= n) return 0;
+        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+        if(s1[idx1] == s2[idx2]){
+            return 1 + solve(idx1+1,idx2+1,n,s1,s2,dp);
+        }
+        return dp[idx1][idx2] = max(solve(idx1+1,idx2,n,s1,s2,dp),solve(idx1,idx2+1,n,s1,s2,dp));
     }
     int longestPalindromeSubseq(string s) {
-
-        memset(t , -1 , sizeof(t));
-        string s2 = s;
-        reverse(begin(s2) , end(s2));
-        int m = s.length();
-        return LCS( s , s2 , m , m);
-        
+        int n = s.length();
+        string s1 = s;
+        reverse(begin(s1),end(s1));
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return solve(0,0,n,s,s1,dp);
     }
 };
