b3b145398c6c93977d4067f084b8cd4084171ca0
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-11-01 23:54:01 +0530

    Time: 91 ms (82.16%), Space: 263.3 MB (76.09%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
@@ -11,27 +11,19 @@
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        unordered_set<int> st(begin(nums) , end(nums));
-
-        while(head != NULL && st.find(head->val) != st.end())
-        {
-            ListNode * temp = head;
-            head = head->next;
-            delete(temp);
-        }
-
-        ListNode * curr = head;
-        while(curr != NULL && curr->next != NULL)
-        {
-            if(st.find(curr->next->val) != st.end())
-            {
-                ListNode * temp = curr->next;
-                curr->next = curr->next->next;
-                delete temp;
+        unordered_set<int> st(nums.begin(),nums.end());
+        ListNode* dummyNode = new ListNode(-1);
+        dummyNode->next = head;
+        ListNode* prev = dummyNode;
+        ListNode* curr = head;
+        while (curr) {
+            if (st.find(curr->val) != st.end()) {
+                prev->next = curr->next;
+            } else {
+                prev = curr;
             }
-            else  curr = curr->next;
+            curr = curr->next;
         }
-        return head;
-        
+        return dummyNode->next;
     }
 };
