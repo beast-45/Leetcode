fde0d914d7a5c1cc7614c07d51e54d18babfa7f7
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-11-01 23:27:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -2,31 +2,11 @@ class Solution {
 public:
     string convert(string s, int numRows) {
         if(numRows == 1) return s;
-        int n = s.length() , j = 0;
-        vector<string> rows(numRows);
+        int n = s.length() , j=0;
         bool down = true;
+        vector<string> words;
         for(int i=0 ; i<n ; i++){
-            rows[j].push_back(s[i]);
-            if(down){
-                if(j == numRows-1){
-                    down = false;
-                    j--;
-                }else{
-                    j++;
-                }
-            }else{
-                if(j == 0){
-                    down = true;
-                    j++;
-                }else{
-                    j--;
-                }
-            }
+            
         }
-        string zigzag = "";
-        for(int i=0 ; i<numRows ; i++){
-            zigzag += rows[i];
-        }
-        return zigzag;
     }
 };
