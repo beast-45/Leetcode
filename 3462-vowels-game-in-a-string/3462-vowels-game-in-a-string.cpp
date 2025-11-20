f3b8b1185636111106e89a7793c2c5a6dfabfc13
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-11-20 18:55:38 +0530

    Time: 7 ms (50.54%), Space: 19.1 MB (56.12%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     bool doesAliceWin(string s) {
-        int vowels = 0;
-        unordered_set<char> st{'a','e','i','o','u'};
         for(char &ch : s){
-            if(st.count(ch)) vowels += 1;
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') return true;
         }
-        return vowels == 0 ? false : true;
+        return false;
     }
 };
