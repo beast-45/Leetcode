06696b85c8eb851342ae24d978665cd5661d0ba8
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-11-21 18:05:34 +0530

    Time: 14 ms (77.05%), Space: 23.3 MB (96.51%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        queue<pair<int,int>> q;
-        vector<vector<int>> directions{{1,0},{-1,0},{0,1},{0,-1},{-1,-1},{-1,1},{1,-1},{1,1}};
-
+        int m = grid.size() , n = grid[0].size();
         if(grid[0][0] == 1 || grid[m-1][n-1] == 1){
             return -1;
         }
-        int path = 0;
+        vector<pair<int,int>> directions{{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+        queue<pair<int,int>> q;
         q.push({0,0});
-        grid[0][0] = -1;
+        grid[0][0] = 1;
+        int path = 0;
         while(!q.empty()){
             int s = q.size();
             while(s--){
-                int x = q.front().first;
-                int y = q.front().second;
+                auto [i,j] = q.front();
                 q.pop();
-                if(x == m-1 && y == n-1){
-                    return path + 1;
-                }
-                for(vector<int> &direction : directions){
-                    int nx = x + direction[0];
-                    int ny = y + direction[1];
-                    if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]==0){
-                        q.push({nx,ny});
-                        grid[nx][ny] = 1;
+                if(i == m-1 && j == n-1) return path+1;
+                for(auto &[di,dj] : directions){
+                    int ni = i + di;
+                    int nj = j + dj;
+                    if(ni>=0 && ni<m && nj>=0 && nj<n && grid[ni][nj] == 0){
+                        q.push({ni,nj});
+                        grid[ni][nj] = 1;
                     }
                 }
             }
-            path+=1;
+            path++;
         }
         return -1;
     }
