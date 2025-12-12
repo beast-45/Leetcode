0dabe07f5c80021f5456f5c9140b0a4b6c3ce25f
manishverma45 <146928373+beast-45@users.noreply.github.com>
2025-12-12 19:50:09 +0530

    Time: 6 ms (17.76%), Space: 22.5 MB (5.31%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    int solve(int index , bool canBuy , int n , vector<int> &prices , vector<vector<int>> &dp){
+    int solve(int index , int n , bool canBuy , vector<int> &prices , vector<vector<int>> &dp){
         if(index == n) return 0;
-        if(dp[index][canBuy] != -1){
-            return dp[index][canBuy];
-        }
+        if(dp[index][canBuy] != -1) return dp[index][canBuy];
         int profit = 0;
         if(canBuy){
-            int buy = -prices[index] + solve(index+1,false,n,prices,dp);
-            int skip = solve(index+1,true,n,prices,dp);
+            int buy = -prices[index] + solve(index+1,n,false,prices,dp);
+            int skip = solve(index+1,n,true,prices,dp);
             profit = max(buy,skip);
         }else{
-            int sell = prices[index] + solve(index+1,true,n,prices,dp);
-            int hold = solve(index+1,false,n,prices,dp);
+            int sell = prices[index] + solve(index+1,n,true,prices,dp);
+            int hold = solve(index+1,n,false,prices,dp);
             profit = max(sell,hold);
         }
         return dp[index][canBuy] = profit;
@@ -20,6 +18,6 @@ public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         vector<vector<int>> dp(n,vector<int>(2,-1));
-        return solve(0,true,n,prices,dp);
+        return solve(0,n,true,prices,dp);
     }
 };
