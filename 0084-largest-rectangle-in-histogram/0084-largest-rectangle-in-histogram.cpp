faaac5f1f1f11218eff3474fad31a383e9e863f1
manishverma45 <146928373+beast-45@users.noreply.github.com>
2026-01-11 18:48:33 +0530

    Time: 28 ms (35.88%), Space: 85.9 MB (23.88%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,15 +1,5 @@
 class Solution {
 public:
-    void makeNSR(int n , vector<int> &nums , vector<int> &nsr){
-        stack<int> st;
-        for(int i=n-1 ; i>=0 ; i--){
-            while(!st.empty() && nums[st.top()] >= nums[i]){
-                st.pop();
-            }
-            nsr[i] = st.empty() ? n : st.top();
-            st.push(i);
-        }
-    }
     void makeNSL(int n , vector<int> &nums , vector<int> &nsl){
         stack<int> st;
         for(int i=0 ; i<n ; i++){
@@ -17,6 +7,16 @@ public:
                 st.pop();
             }
             nsl[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+    }
+    void makeNSR(int n , vector<int> &nums , vector<int> &nsr){
+        stack<int> st;
+        for(int i=n-1 ; i>=0 ; i--){
+            while(!st.empty() && nums[st.top()] >= nums[i]){
+                st.pop();
+            }
+            nsr[i] = st.empty() ? n : st.top();
             st.push(i);
         }
     }
