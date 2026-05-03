4e904c241a1ce80c7e470c4758bbb5ae996e1e36
manishverma45 <146928373+beast-45@users.noreply.github.com>
2026-05-03 11:03:19 +0530

    Time: 63 ms (54.9%), Space: 182.4 MB (28.61%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -1,39 +1,35 @@
 class NumArray {
 public:
     int n;
-    vector<int> segTree;
+    vector<int> segtree;
     void buildSegTree(int i , int l , int r , vector<int> &nums){
         if(l == r){
-            segTree[i] = nums[l];
+            segtree[i] = nums[l];
             return;
         }
         int mid = l+(r-l)/2;
         buildSegTree(2*i+1,l,mid,nums);
         buildSegTree(2*i+2,mid+1,r,nums);
-        segTree[i] = segTree[2*i+1] + segTree[2*i+2];
+        segtree[i] = segtree[2*i+1] + segtree[2*i+2];
     }
     void updateSegTree(int index , int value , int i , int l , int r){
         if(l == r){
-            segTree[i] = value;
+            segtree[i] = value;
             return;
         }
         int mid = l+(r-l)/2;
         index <= mid ? updateSegTree(index,value,2*i+1,l,mid) : updateSegTree(index,value,2*i+2,mid+1,r);
-        segTree[i] = segTree[2*i+1] + segTree[2*i+2];
+        segtree[i] = segtree[2*i+1] + segtree[2*i+2];
     }
     int querySegTree(int start , int end , int i , int l , int r){
-        if(l>end || r<start){
-            return 0;
-        }
-        if(l>=start && r<=end){
-            return segTree[i];
-        }
+        if(l>end || r<start) return 0;
+        if(l>=start && r<=end) return segtree[i];
         int mid = l+(r-l)/2;
         return querySegTree(start,end,2*i+1,l,mid) + querySegTree(start,end,2*i+2,mid+1,r);
     }
     NumArray(vector<int>& nums) {
         n = nums.size();
-        segTree.resize(4*n);
+        segtree.resize(4*n);
         buildSegTree(0,0,n-1,nums);
     }
     
@@ -45,3 +41,10 @@ public:
         return querySegTree(left,right,0,0,n-1);
     }
 };
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
