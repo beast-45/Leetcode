3cd1981d1af4a9ca0b2f08f7a585fff0c11dea03
manishverma45 <146928373+beast-45@users.noreply.github.com>
2026-05-29 19:45:51 +0530

    Time: 11 ms (26.19%), Space: 38.9 MB (18.39%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -11,7 +11,24 @@
  */
 class Solution {
 public:
+    vector<vector<int>> result;
+    void fill(TreeNode* root , int sum , vector<int> temp , int targetSum)
+    {
+        if(root == NULL) return;
+        sum += root->val;
+        temp.push_back(root->val);
+        if(root->left == NULL && root->right == NULL)
+        {
+            if(sum == targetSum) result.push_back(temp);
+        }
+        fill(root->left , sum , temp , targetSum);
+        fill(root->right , sum , temp , targetSum);
+    }
+
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        
+        int sum = 0;
+        vector<int> temp;
+        fill(root , sum , temp , targetSum);
+        return result;
     }
 };
