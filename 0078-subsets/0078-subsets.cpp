7574bb825d2946094eab9cd55fcd56fa8303127c
manishverma45 <146928373+beast-45@users.noreply.github.com>
2026-06-01 20:43:44 +0530

    Time: 0 ms (100%), Space: 10 MB (58.28%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    void solve(int index , vector<int> &temp , vector<vector<int>> &result , vector<int> &nums){
-        result.push_back(temp);
-        for(int i=index ; i<nums.size() ; i++){
-            temp.push_back(nums[i]);
-            solve(i+1,temp,result,nums);
-            temp.pop_back();
+    void makeSubsets(int index , vector<int> &nums , vector<int> &temp , vector<vector<int>> &result){
+        if(index >= nums.size()){
+            result.push_back(temp);
+            return;
         }
+        temp.push_back(nums[index]);
+        makeSubsets(index+1 , nums , temp , result);
+        temp.pop_back();
+        makeSubsets(index+1 , nums , temp , result);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
         vector<int> temp;
         vector<vector<int>> result;
-        solve(0,temp,result,nums);
-        return result;
+        makeSubsets(0 , nums , temp , result);
+        return result;  
     }
 };
