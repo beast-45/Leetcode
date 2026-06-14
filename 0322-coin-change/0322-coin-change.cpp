d25161e7141eba54c282fce0e6c7e83ba95e8732
manishverma45 <146928373+beast-45@users.noreply.github.com>
2026-06-15 01:32:01 +0530

    Time: 51 ms (23.16%), Space: 53.9 MB (7.5%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     int solve(int index , int n , int amount , vector<int> &nums , vector<vector<int>> &dp){
-        if(amount == 0){
-            return 0;
-        }
+        if(amount == 0) return 0;
         if(amount < 0 || index >= n) return 1e6;
-        if(dp[index][amount] != -1){
-            return dp[index][amount];
-        }
+        if(dp[index][amount] != -1) return dp[index][amount];
         int take = 1 + solve(index,n,amount-nums[index],nums,dp);
         int skip = solve(index+1,n,amount,nums,dp);
         return dp[index][amount] = min(take,skip);
@@ -15,7 +11,7 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
-        int minCoins = solve(0,n,amount,coins,dp);
-        return minCoins == 1e6 ? -1 : minCoins;
+        int result = solve(0,n,amount,coins,dp);
+        return result >= 1e6 ? -1 : result;
     }
 };
